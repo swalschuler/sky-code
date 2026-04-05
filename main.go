0de1198ef9e9f681e876b99028dbf31cdee66e15
Schuyler Alschuler <alschuler.s@gmail.com>
2026-04-05 10:49:39 -0700

    Dummy tool.

--- main.go
+++ main.go
@@ -10,6 +10,25 @@ import (
 	"github.com/anthropics/anthropic-sdk-go"
 )
 
+var GetCoordinatesInputSchema = anthropic.ToolInputSchemaParam{
+	Type: "object",
+	Properties: map[string]interface{}{
+		"location": map[string]interface{}{
+			"type":        "string",
+			"description": "The name of the place or address to get coordinates for.",
+		},
+	},
+	Required: []string{"location"},
+}
+
+var toolParams = []anthropic.ToolParam{
+	{
+		Name:        "get_coordinates",
+		Description: anthropic.String("Accepts a place as an address, then returns the latitude and longitude coordinates."),
+		InputSchema: GetCoordinatesInputSchema,
+	},
+}
+
 func main() {
 	client := anthropic.NewClient()
 
@@ -21,7 +40,12 @@ func main() {
 		return "", false
 	}
 
-	agent := Agent{client: client, getInput: getInput}
+	var tools = make([]anthropic.ToolUnionParam, len(toolParams))
+	for i, toolParam := range toolParams {
+		tools[i] = anthropic.ToolUnionParam{OfTool: &toolParam}
+	}
+
+	agent := Agent{client: client, getInput: getInput, tools: tools}
 	agent.Run()
 
 }
@@ -29,6 +53,7 @@ func main() {
 type Agent struct {
 	client   anthropic.Client
 	getInput func() (string, bool)
+	tools    []anthropic.ToolUnionParam
 }
 
 func (a *Agent) Run() {
@@ -41,22 +66,62 @@ func (a *Agent) Run() {
 		if ok {
 			fmt.Println("Got input:", input)
 			messages = append(messages, anthropic.NewUserMessage(anthropic.NewTextBlock(input)))
-			message := a.runInference(messages)
-			messages = append(messages, message.ToParam())
 
-			print("[assistant]: ")
-			for _, block := range message.Content {
-				switch block := block.AsAny().(type) {
-				case anthropic.TextBlock:
-					println(block.Text)
-					println()
-				case anthropic.ToolUseBlock:
-					inputJSON, _ := json.Marshal(block.Input)
-					println(block.Name + ": " + string(inputJSON))
-					println()
+			for {
+				message := a.runInference(messages)
+
+				print("[assistant]: ")
+				for _, block := range message.Content {
+					switch block := block.AsAny().(type) {
+					case anthropic.TextBlock:
+						println(block.Text)
+						println()
+					case anthropic.ToolUseBlock:
+						inputJSON, _ := json.Marshal(block.Input)
+						println(block.Name + ": " + string(inputJSON))
+						println()
+					}
 				}
-			}
 
+				messages = append(messages, message.ToParam())
+				toolResults := []anthropic.ContentBlockParamUnion{}
+
+				for _, block := range message.Content {
+					switch variant := block.AsAny().(type) {
+					case anthropic.ToolUseBlock:
+						print("[user (" + block.Name + ")]: ")
+
+						var response interface{}
+						switch block.Name {
+						case "get_coordinates":
+							var input struct {
+								Location string `json:"location"`
+							}
+
+							err := json.Unmarshal([]byte(variant.JSON.Input.Raw()), &input)
+							if err != nil {
+								panic(err)
+							}
+
+							response = GetCoordinates(input.Location)
+						}
+
+						b, err := json.Marshal(response)
+						if err != nil {
+							panic(err)
+						}
+
+						println(string(b))
+
+						toolResults = append(toolResults, anthropic.NewToolResultBlock(block.ID, string(b), false))
+					}
+
+				}
+				if len(toolResults) == 0 {
+					break
+				}
+				messages = append(messages, anthropic.NewUserMessage(toolResults...))
+			}
 		} else {
 			fmt.Println("End of input.")
 		}
@@ -64,11 +129,16 @@ func (a *Agent) Run() {
 	}
 }
 
+func GetCoordinates(s string) interface{} {
+	return map[string]int{"lat": 1, "long": 2}
+}
+
 func (a *Agent) runInference(messages []anthropic.MessageParam) *anthropic.Message {
 	message, err := a.client.Messages.New(context.TODO(), anthropic.MessageNewParams{
 		Model:     anthropic.ModelClaudeOpus4_6,
 		Messages:  messages,
 		MaxTokens: 1024,
+		Tools:     a.tools,
 	})
 	if err != nil {
 		panic(err)
